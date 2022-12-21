cafdd67ed04efbf1a1de169841a6ba66900c117d
KimDaeyeong <daeyoung4088@hanmail.net>
2022-12-21 17:32:30 +0900

    [feat]Entity Init

---
+++ backend/toy1/src/main/java/com/example/toy1/domain/User.java
@@ -0,0 +1,40 @@
+package com.example.toy1.domain;
+
+import com.example.toy1.domain.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "uid")
+    private Long uid;
+
+    @Column(name = "user_id")
+    private String id;
+
+    @Column(name = "user_pw")
+    private String pw;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "role")
+    private Role role;
+
+
+}
---
+++ backend/toy1/src/main/java/com/example/toy1/domain/enums/Role.java
@@ -0,0 +1,5 @@
+package com.example.toy1.domain.enums;
+
+public enum Role {
+    USER, ADMIN
+}
