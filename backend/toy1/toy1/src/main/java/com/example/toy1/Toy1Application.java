c070c39d5b1f97789d9146594ce97140becfb53b
KimDaeyeong <daeyoung4088@hanmail.net>
2022-12-21 17:10:42 +0900

    backend structure

---
+++ backend/toy1/toy1/src/main/java/com/example/toy1/Toy1Application.java
@@ -0,0 +1,13 @@
+package com.example.toy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toy1Application.class, args);
+	}
+
+}
