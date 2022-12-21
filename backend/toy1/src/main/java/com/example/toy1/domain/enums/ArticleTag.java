7c7b3b4d07d037a5525d1d2c88e4e9d91639727a
Wondo-K <wondok95@naver.com>
2022-12-21 19:34:51 +0900

    [style] enums 오타 ë및 컨벤션 수정

--- backend/toy1/src/main/java/com/example/toy1/domain/enums/ArticleTag.java
+++ backend/toy1/src/main/java/com/example/toy1/domain/enums/ArticleTag.java
@@ -1,5 +1,5 @@
 package com.example.toy1.domain.enums;
 
 public enum ArticleTag {
-    KBO,DOOSAN,LG,KIWOOM,SSG,KT,HANHWA,SAMSUNG,KIA,LOTTE,NC
+    KBO, DOOSAN, LG, KIWOOM, SSG, KT, HANHWA, SAMSUNG, KIA, LOTTE, NC
 }
--- backend/toy1/src/main/java/com/example/toy1/domain/enums/InquiryTag.java
+++ backend/toy1/src/main/java/com/example/toy1/domain/enums/InquiryTag.java
@@ -1,5 +1,5 @@
 package com.example.toy1.domain.enums;
 
 public enum InquiryTag {
-    INQIRY, REPORT
+    INQUIRY, REPORT
 }
