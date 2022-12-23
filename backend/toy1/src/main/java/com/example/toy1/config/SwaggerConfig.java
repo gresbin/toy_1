336250585b590acc253c27d759097341965909df
KimDaeyeong <daeyoung4088@hanmail.net>
2022-12-23 14:33:08 +0900

    [feat]swagger config update

---
+++ backend/toy1/src/main/java/com/example/toy1/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.toy1.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        Info info = new Info().title("").version("1")
+                .description("")
+                .termsOfService("http://swagger.io/terms/")
+                .contact(new Contact().name("").email(""))
+                .license(new License().name("").url(""));
+
+        return new OpenAPI()
+                .info(info)
+                .components(new Components()
+                        .addSecuritySchemes("Authorization", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")))
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"));
+    }
+
+}
