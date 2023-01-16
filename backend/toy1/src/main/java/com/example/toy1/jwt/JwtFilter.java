ecfda508a2f0ec95f86154de35244cd6b121f692
KimDaeyeong <daeyoung4088@hanmail.net>
2023-01-16 20:52:13 +0900

    [fix] tãest print

--- backend/toy1/src/main/java/com/example/toy1/jwt/JwtFilter.java
+++ backend/toy1/src/main/java/com/example/toy1/jwt/JwtFilter.java
@@ -25,14 +25,18 @@ public class JwtFilter extends GenericFilterBean {
 
     // JWT 토큰의 정보 인증 정보를 현재 스레드의 SecurityContext에 저장하는 역할
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
         // Request 헤더에서 토큰 정보꺼내기
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         String jwt = resolveToken(httpServletRequest);
         String requestURI = httpServletRequest.getRequestURI();
+        System.out.println("정보 꺼내보자");
 
         // 유효성 검사 후 정상이면 Authentication을 SecurityContext에 저장
         if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            System.out.println("성공했냐?");
             Authentication authentication = tokenProvider.getAuthentication(jwt);
             SecurityContextHolder.getContext().setAuthentication(authentication);
             System.out.println("Security Context에 " + authentication.getName() + " 인증 정보를 저장했습니다. uri : " + requestURI);
